adfa9200e1e50a2a33b4dd982060a0a82f09a8ab
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-01-05 10:02:45 +0900

    Test `AtomicIndices.jl`.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,11 @@ include("../src/utils/RotationMatrices.jl")
 using Magesty
 using Test
 
-include("./test_SortedContainer.jl")
-include("./test_AtomicIndices.jl")
-include("./test_UnitaryMatrixCl.jl")
-include("./test_RotationMatrices.jl")
-
+@testset "Component tests" begin
+	include("./test_SortedContainer.jl")
+	include("./test_AtomicIndices.jl")
+	include("./test_UnitaryMatrixCl.jl")
+	include("./test_RotationMatrices.jl")
+end
 # @testset "Magesty.jl" begin
 # end
--- test/test_AtomicIndices.jl
+++ test/test_AtomicIndices.jl
@@ -1,4 +1,4 @@
-
+using .SortedContainer
 using .AtomicIndices
 
 @testset "AtomicIndices" begin
@@ -22,4 +22,30 @@ using .AtomicIndices
 	@test iul1 == iul1
 	iul2 = IndicesUniqueList([indices1, indices3])
 	@test iul2 < iul1
+
+	scv = SortedCountingVector([iul1, iul1, iul2])
+	scv2 = SortedCountingVector([iul1, iul2, iul2])
+	@test scv[1] == iul2
+	@test scv[2] == iul1
+	@test length(scv) == 2
+	@test scv == sort([iul1, iul2])
+	@test sort([iul1, iul2]) == scv
+	@test scv == scv
+	@test scv != scv2
+	@test getcount(scv, iul1) == 2
+	@test getcount(scv, iul2) == 1
+	push!(scv, iul1)
+	@test getcount(scv, iul1) == 3
+	append!(scv2, [iul1, iul2])
+	@test getcount(scv2, iul1) == 2
+	@test getcount(scv2, iul2) == 3
+	delete!(scv2, iul2)
+	@test iul1 in scv2
+	@test !(iul2 in scv2)
+
+	scv3 = SortedCountingVector([iul1, iul1, iul2])
+	scv3_copy = copy(scv3)
+	push!(scv3_copy, iul1)
+	@test scv3 != scv3_copy
 end
+
