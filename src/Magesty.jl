b86057b0bee70dd50b9b4edaa6e37631e482f8c3
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-01-05 13:10:42 +0900

    Refactor `Symmetry.jl`. Arrange test directory.

--- src/Magesty.jl
+++ src/Magesty.jl
@@ -2,6 +2,11 @@ module Magesty
 
 include("common/SortedContainer.jl")
 
+include("types/AtomCells.jl")
 include("types/AtomicIndices.jl")
+include("types/UnitaryMatrixCl.jl")
+
+include("Systems.jl")
+include("Symmetry.jl")
 
 end
