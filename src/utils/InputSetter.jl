3cb3e78d6856292d063e52389fbdc88ef1f5e54d
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-01-06 11:49:54 +0900

    Add `InputParser.jl`, `InputSetter.jl`. Debug whole files.

---
+++ src/utils/InputSetter.jl
@@ -0,0 +1,35 @@
+function set_system(parser)
+	system = System(
+		parser.lattice_vectors,
+		parser.is_periodic,
+		parser.kd_name,
+		parser.kd_int_list,
+		parser.x_fractional)
+end
+
+function set_symmetry(parser, system)
+	symmetry = Symmetry(
+		system,
+		parser.tolerance_sym,
+	)
+end
+
+function set_cluster(parser, system, symmetry)
+	cluster = Cluster(system, symmetry, parser.nbody, parser.cutoff_radii)
+end
+
+function set_sccoeff(parser, system, symmetry, cluster)
+	sccoeff = SCcoeff(system, symmetry, cluster, parser.lmax, parser.nbody)
+end
+
+function set_optimize(parser, system, symmetry, cluster)
+	optimize = Optimize(
+		system,
+		symmetry,
+		cluster,
+		parser.time_reversal_sym,
+		parser.crystal_sym,
+		parser.lmax,
+		parser.model,
+	)
+end
