470b63cd549b8d70e2b52894ef3c6f53d0e9f310
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-01-13 11:48:34 +0900

    Add `equivalent_atom_list` field in `Cluster.jl`.

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,13 +17,13 @@ using Test
 end
 
 @testset "examples" begin
-	include("./examples/b2feco1x1x1.jl")
+	# include("./examples/b2feco1x1x1.jl")
 	# include("./examples/bccfe1x1x1.jl")
-	# include("./examples/b2feco1x1x2.jl")
+	include("./examples/b2feco1x1x2.jl")
 	# include("./examples/bccfe2x2x2.jl")
 	# include("./examples/b2feco2x2x2.jl")
 	# include("./examples/fesi_atomicchain.jl")
-	include("./examples/hcpco1x1x1.jl")
+	# include("./examples/hcpco1x1x1.jl")
 end
 # @testset "Magesty.jl" begin
 # end
