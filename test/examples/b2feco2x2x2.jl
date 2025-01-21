8c54377a96cac711e0686fb8fffa45f0e56d8a3e
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-01-21 14:23:28 +0900

    modify `is_translationally_equiv_basis` function to adapt the new Indices struct

--- test/examples/b2feco2x2x2.jl
+++ test/examples/b2feco2x2x2.jl
@@ -19,9 +19,9 @@ using TOML
 		Fe = [ 0, 1 ] # the number of elements shoud be the same with "nbody" value.
 		Co = [ 0, 1 ]
 		[interaction.cutoff] # unit is bohr
-		Fe-Fe = [ 0, 0 ] # first element is just dummy to align wigh lmax array
-		Fe-Co = [ 0, 3 ]
-		Co-Co = [ 0, 0 ]
+		Fe-Fe = [ 0, -1 ] # first element is just dummy to align wigh lmax array
+		Fe-Co = [ 0, -1 ]
+		Co-Co = [ 0, -1 ]
 		# negative cutoff means all of the possible interaction will be considered.
 
 	[regression]
