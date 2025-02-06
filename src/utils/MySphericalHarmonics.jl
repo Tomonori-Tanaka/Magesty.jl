0ea4e64fa69ed0cfa5962c5ec72a74292a00d236
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-02-06 11:28:27 +0900

    add check the norm of uvec in `MySphericalHarmonics.jl`.

--- src/utils/MySphericalHarmonics.jl
+++ src/utils/MySphericalHarmonics.jl
@@ -1,5 +1,5 @@
 """
-    module MySphericalHarmonics
+	module MySphericalHarmonics
 
 This module provides functions to compute spherical harmonics ( Y_{l,m} ) and related derivatives, following the formalism described in Drautz (Phys. Rev. B 102, 024104, 2020). It includes:
 
@@ -46,6 +46,10 @@ function dP̄ₗₘ(l::Int, m::Int, r̂z::Real)::Float64
 end
 
 function dP̄ₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return (-1)^m * √((2l + 1) / (4π) * factorial(l - m) / factorial(l + m)) *
 		   dnPl(uvec[3], l, m + 1)
 end
@@ -54,6 +58,10 @@ end
 equation (D18) in R. Drautz, Phys. Rev. B 102, 024104 (2020).
 """
 function ∂Yₗₘ_∂r̂x(l::Int, m::Int, uvec::Vector{<:Real})::Complex
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return m * (uvec[1] + uvec[2] * im)^(m - 1) * P̄ₗₘ(l, m, uvec[3])
 end
 
@@ -61,6 +69,10 @@ end
 equation (D19) in R. Drautz, Phys. Rev. B 102, 024104 (2020).
 """
 function ∂Yₗₘ_∂r̂y(l::Int, m::Int, uvec::Vector{<:Real})::Complex
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return 1.0im * ∂Yₗₘ_∂r̂x(l, m, uvec)
 end
 
@@ -68,6 +80,10 @@ end
 equation (D20) in R. Drautz, Phys. Rev. B 102, 024104 (2020).
 """
 function ∂Yₗₘ_∂r̂z(l::Int, m::Int, uvec::Vector{<:Real})::Complex
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return (uvec[1] + uvec[2] * im)^m * dnPl(l, m + 1, uvec[3])
 end
 
@@ -76,11 +92,19 @@ ref)
 equation (D22) in R. Drautz, Phys. Rev. B 102, 024104 (2020).
 """
 function yₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Complex
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return uvec[1] * ∂Yₗₘ_∂r̂x(l, m, uvec) + uvec[2] * ∂Yₗₘ_∂r̂y(l, m, uvec) +
 		   uvec[3] * ∂Yₗₘ_∂r̂z(l, m, uvec)
 end
 
 function Yₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Complex
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	if m < 0
 		return (-1)^(abs(m)) * conj(uvec[1] + uvec[2] * im)^abs(m) *
 			   P̄ₗₘ(l, abs(m), uvec[3])
@@ -92,6 +116,10 @@ end
 function Sₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Float64
 	if l ≤ 0
 		error("Invalid value for l: l must be greater than 0 in the Sₗₘ function.")
+	end
+
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
 	end
 
 	if m == 0
@@ -113,6 +141,10 @@ function Sₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Float64
 end
 
 function ∂Sₗₘ_∂r̂x(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	if m == 0
 		return 0.0
 	elseif m > 0
@@ -137,24 +169,24 @@ function ∂Sₗₘ_∂r̂x(l::Int, m::Int, uvec::Vector{<:Real})::Float64
 end
 
 function ∂Sₗₘ_∂r̂y(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	if m == 0
 		return 0.0
 	elseif m > 0
 		return -∂Sₗₘ_∂r̂x(l, -m, uvec)
-		#= 		if m == 1
-					return 0.0
-				else
-					return (-1)^(m+1) * √2 * m * P̄ₗₘ(l, m, uvec[3]) *
-						sum(
-							(-1)^k * binomial(m-1, 2k + 1) * uvec[1]^(m-2k-2) * uvec[2]^(2k+1) for k in 0:floor(Int, (m-2)/2)
-						)
-				end =#
 	else # m < 0
 		return ∂Sₗₘ_∂r̂x(l, abs(m), uvec)
 	end
 end
 
 function ∂Sₗₘ_∂r̂z(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	if m == 0
 		return dP̄ₗₘ(l, m, uvec[3])
 	elseif m > 0
@@ -175,27 +207,60 @@ end
 
 # ss imply "small s"
 function ssₗₘ(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return uvec[1] * ∂Sₗₘ_∂r̂x(l, m, uvec) +
 		   uvec[2] * ∂Sₗₘ_∂r̂y(l, m, uvec) +
 		   uvec[3] * ∂Sₗₘ_∂r̂z(l, m, uvec)
 end
 
 function ∂Sₗₘ_∂x(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return ∂Sₗₘ_∂r̂x(l, m, uvec) - uvec[1] * ssₗₘ(l, m, uvec)
 end
 
 function ∂Sₗₘ_∂y(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return ∂Sₗₘ_∂r̂y(l, m, uvec) - uvec[2] * ssₗₘ(l, m, uvec)
 end
 
 function ∂Sₗₘ_∂z(l::Int, m::Int, uvec::Vector{<:Real})::Float64
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return ∂Sₗₘ_∂r̂z(l, m, uvec) - uvec[3] * ssₗₘ(l, m, uvec)
 end
 
 d_Slm = [∂Sₗₘ_∂x, ∂Sₗₘ_∂y, ∂Sₗₘ_∂z]
 
 function ∂ᵢSlm(l::Int, m::Int, uvec::Vector{<:Real})::Vector{Float64}
+	if !is_normalized(uvec)
+		error("uvec is not normalized")
+	end
+
 	return [∂Sₗₘ_∂x(l, m, uvec), ∂Sₗₘ_∂y(l, m, uvec), ∂Sₗₘ_∂z(l, m, uvec)]
+end
+
+function is_normalized(vec::AbstractVector{<:Real}, tol::Real = 1e-8)::Bool
+	if length(vec) != 3
+		error("vec must be a 3-element vector")
+	end
+
+	norm_vec = norm(vec)
+	if isapprox(norm_vec, 1.0, atol = tol)
+		return true
+	else
+		return false
+	end
 end
 
 end
