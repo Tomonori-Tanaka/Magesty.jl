bb3c4d40d3e59e50607dc80da4b6ce5b97d846d5
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-02-17 12:01:13 +0900

    modify `Base.show` function in SALC.jl show `multiplicity` field

--- src/types/SALC.jl
+++ src/types/SALC.jl
@@ -1,5 +1,5 @@
 """
-    Module SALCs
+	Module SALCs
 
 This module provides a data structure for symmetry-adapted linear combinations (SALCs).
 """
@@ -14,45 +14,48 @@ import Base: show
 export SALC
 
 struct SALC
-    basisset::Vector{IndicesUniqueList}
-    coeffs::Vector{Float64}
-    multiplicity::Vector{Int}
+	basisset::Vector{IndicesUniqueList}
+	coeffs::Vector{Float64}
+	multiplicity::Vector{Int}
 end
 
-function SALC(basislist::SortedCountingUniqueVector{IndicesUniqueList}, coeffs::Vector{<:Real})
-    if length(basislist) != length(coeffs)
-        throw(ArgumentError("The length of basislist and coeffs must be the same."))
-    end
+function SALC(
+	basislist::SortedCountingUniqueVector{IndicesUniqueList},
+	coeffs::Vector{<:Real},
+)
+	if length(basislist) != length(coeffs)
+		throw(ArgumentError("The length of basislist and coeffs must be the same."))
+	end
 
-    result_basisset = Vector{IndicesUniqueList}()
-    result_coeffs = Vector{Float64}()
-    result_multiplicity = Vector{Int}()
+	result_basisset = Vector{IndicesUniqueList}()
+	result_coeffs = Vector{Float64}()
+	result_multiplicity = Vector{Int}()
 
-    for (idx, basis) in enumerate(basislist)
-        count::Int = basislist.counts[basis]
-        coeff::Float64 = coeffs[idx] * count
-        if !isapprox(coeff, 0.0, atol = 1e-8)
-            push!(result_basisset, basis)
-            push!(result_coeffs, coeff)
-            push!(result_multiplicity, count)
-        end
-    end
+	for (idx, basis) in enumerate(basislist)
+		count::Int = basislist.counts[basis]
+		coeff::Float64 = coeffs[idx] * count
+		if !isapprox(coeff, 0.0, atol = 1e-8)
+			push!(result_basisset, basis)
+			push!(result_coeffs, coeff)
+			push!(result_multiplicity, count)
+		end
+	end
 
-    # normalize coefficient vector
-    norm_coeffs = norm(result_coeffs)
-    if isapprox(norm_coeffs, 0.0, atol = 1e-8)
-        throw(ArgumentError("The norm of the coefficient vector is zero."))
-    end
-    result_coeffs ./= norm_coeffs
+	# normalize coefficient vector
+	norm_coeffs = norm(result_coeffs)
+	if isapprox(norm_coeffs, 0.0, atol = 1e-8)
+		throw(ArgumentError("The norm of the coefficient vector is zero."))
+	end
+	result_coeffs ./= norm_coeffs
 
-    return SALC(result_basisset, result_coeffs, result_multiplicity)
+	return SALC(result_basisset, result_coeffs, result_multiplicity)
 end
 
 function show(io::IO, salc::SALC)
-    println(io, "number of SALCs: ", length(salc.basisset))
-    for (basis, coeff) in zip(salc.basisset, salc.coeffs)
-        println(io, coeff, "\t", basis)
-    end
+	println(io, "number of SALCs: ", length(salc.basisset))
+	for (basis, coeff, multiplicity) in zip(salc.basisset, salc.coeffs, salc.multiplicity)
+		println(io, multiplicity, "\t", coeff, "\t", basis)
+	end
 end
 
 end
