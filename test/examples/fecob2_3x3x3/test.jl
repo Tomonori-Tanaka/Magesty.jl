66d55e7bbf555bac87ea009eac44193bebd72995
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-02-18 11:30:59 +0900

    add new test fecob2_3x3x3

---
+++ test/examples/fecob2_3x3x3/test.jl
@@ -0,0 +1,10 @@
+using TOML
+
+input = TOML.parse(open("/Users/tomorin/Packages/Magesty/test/examples/fecob2_3x3x3/input.toml", "r"))
+sclus = SpinCluster(input)
+
+@show sclus.cluster.cluster_list_with_cell
+@show sclus.basisset.basislist
+display(sclus.basisset.classified_basisdict)
+display(sclus.basisset.salc_list)
+display(sclus.optimize.SCE)
