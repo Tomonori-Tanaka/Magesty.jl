3f1f7231a545393a1394eb06e69cbed1547df2be
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-04-18 11:56:49 +0900

    refactor `Magesty.jl`

--- test/examples/febcc_2x2x2_fm/test.jl
+++ test/examples/febcc_2x2x2_fm/test.jl
@@ -3,6 +3,7 @@ using TOML
 input = TOML.parse(
 	open("/Users/tomorin/Packages/Magesty/test/examples/febcc_2x2x2_fm/input.toml", "r"),
 )
-sclus = SpinCluster(input)
+system = System(input)
+sclus = SpinCluster(system)
 print_info(sclus)
 display(sclus.optimize.SCE)
