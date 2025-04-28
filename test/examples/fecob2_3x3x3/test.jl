6e7ebcca0ae8a41e8546f57bd54eb1e446c62803
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-04-28 18:35:05 +0900

    modify some test files

--- test/examples/fecob2_3x3x3/test.jl
+++ test/examples/fecob2_3x3x3/test.jl
@@ -1,10 +1,15 @@
 using TOML
 
 input = TOML.parse(open("/Users/tomorin/Packages/Magesty/test/examples/fecob2_3x3x3/input.toml", "r"))
-system = System(input)
-sclus = SpinCluster(system)
-display(sclus.optimize.SCE)
+# system = System(input)
+# sclus = SpinCluster(system, false)
+# display(sclus.optimize.SCE)
 input["regression"]["weight"] = 1.0
-system_torque = System(input)
-sclus_torque = SpinCluster(system_torque)
-display(sclus_torque.optimize.SCE)
+system_torque = System(input, false)
+sclus_torque = SpinCluster(system_torque, false)
+println("1NN (torque): ", sclus_torque.optimize.SCE[2])
+println(@sprintf("elapsed_time (structure): %10.6f", sclus_torque.structure.elapsed_time))
+println(@sprintf("elapsed_time (symmetry):  %10.6f", sclus_torque.symmetry.elapsed_time))
+println(@sprintf("elapsed_time (cluster):   %10.6f", sclus_torque.cluster.elapsed_time))
+println(@sprintf("elapsed_time (basisset):  %10.6f", sclus_torque.basisset.elapsed_time))
+println(@sprintf("elapsed_time (optimize):  %10.6f", sclus_torque.optimize.elapsed_time))
