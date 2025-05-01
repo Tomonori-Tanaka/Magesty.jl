37de4011ec0775cc2f25b6793337520cd1702a59
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-05-01 12:14:21 +0900

    implement nonlinear optimization for SCE in `Optimize.jl`

--- test/examples/febcc_2x2x2_pm/test.jl
+++ test/examples/febcc_2x2x2_pm/test.jl
@@ -4,20 +4,19 @@ using Printf
 input = TOML.parse(
 	open("/Users/tomorin/Packages/Magesty/test/examples/febcc_2x2x2_pm/input.toml", "r"),
 )
-input["regression"]["weight"] = 0.0
 system = System(input, false)
-sclus_torque = SpinCluster(system, false)
-input["regression"]["weight"] = 1.0
-sclus_energy = SpinCluster(system, input, false)
+weight_list = collect(0.0:0.2:1.0)
+for weight in weight_list
+	println("weight: ", weight)
+	input["regression"]["weight"] = weight
+	sclus = SpinCluster(system, input, false)
+	println("1NN: ", sclus.optimize.SCE[2])	
+	println("relative error energy:            ", sclus.optimize.relative_error_energy)
+	println("relative error magfield_vertical: ", sclus.optimize.relative_error_magfield_vertical)
+end
 
-println("1NN (torque): ", sclus_torque.optimize.SCE[2])
-println("1NN (energy): ", sclus_energy.optimize.SCE[2])
-println("relative error energy (energy): ", sclus_energy.optimize.relative_error_energy)
-println("relative error energy (magfield_vertical): ", sclus_energy.optimize.relative_error_magfield_vertical)
-println("relative error torque (energy): ", sclus_torque.optimize.relative_error_energy)
-println("relative error torque (magfield_vertical): ", sclus_torque.optimize.relative_error_magfield_vertical)
-println(@sprintf("elapsed_time (structure): %10.6f", sclus_torque.structure.elapsed_time))
-println(@sprintf("elapsed_time (symmetry):  %10.6f", sclus_torque.symmetry.elapsed_time))
-println(@sprintf("elapsed_time (cluster):   %10.6f", sclus_torque.cluster.elapsed_time))
-println(@sprintf("elapsed_time (basisset):  %10.6f", sclus_torque.basisset.elapsed_time))
-println(@sprintf("elapsed_time (optimize):  %10.6f", sclus_torque.optimize.elapsed_time))
+# println(@sprintf("elapsed_time (structure): %10.6f", sclus_torque.structure.elapsed_time))
+# println(@sprintf("elapsed_time (symmetry):  %10.6f", sclus_torque.symmetry.elapsed_time))
+# println(@sprintf("elapsed_time (cluster):   %10.6f", sclus_torque.cluster.elapsed_time))
+# println(@sprintf("elapsed_time (basisset):  %10.6f", sclus_torque.basisset.elapsed_time))
+# println(@sprintf("elapsed_time (optimize):  %10.6f", sclus_torque.optimize.elapsed_time))
