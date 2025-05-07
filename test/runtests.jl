5f137e8128d8180a3c1d625d5bfb0b0c60d21157
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-05-07 09:53:06 +0900

    test: update runtests.jl to conditionally execute test sets based on test mode. Add tests for unit, integration, and development modes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,23 +11,32 @@ include("../src/utils/ConfigParser.jl")
 using Magesty
 using Test
 
-@testset "component tests" begin
-	include("./component_test/test_MySphericalHarmonics.jl")
-	include("./component_test/test_SortedContainer.jl")
-	include("./component_test/test_CountingContainer.jl")
-	include("./component_test/test_AtomicIndices.jl")
-	include("./component_test/test_UnitaryMatrixCl.jl")
-	include("./component_test/test_RotationMatrix.jl")
-	include("./component_test/test_SpinConfig.jl")
-	include("./component_test/test_ConfigParser.jl")
+test_mode = get(ENV, "TEST_MODE", "all")
 
-	include("./component_test/test_Structure.jl")
-	include("./component_test/test_Symmetry.jl")
-end 
+if test_mode in ("unit", "all")
+	@testset "component tests" begin
+		include("./component_test/test_MySphericalHarmonics.jl")
+		include("./component_test/test_SortedContainer.jl")
+		include("./component_test/test_CountingContainer.jl")
+		include("./component_test/test_AtomicIndices.jl")
+		include("./component_test/test_UnitaryMatrixCl.jl")
+		include("./component_test/test_RotationMatrix.jl")
+		include("./component_test/test_SpinConfig.jl")
+		include("./component_test/test_ConfigParser.jl")
+		include("./component_test/test_Structure.jl")
+	end
 
-@testset "examples" begin
-	include("./examples/febcc_2x2x2_pm/test.jl")
+	if test_mode in ("integration", "all")
+		@testset "examples" begin
+			include("./examples/febcc_2x2x2_pm/test.jl")
 
-	include("./develop_tmp/febcc_2x2x2_pm/test.jl")
-	include("./develop_tmp/fept_tetragonal_2x2x2/test.jl")
+		end
+	end
+end
+
+if test_mode in ("develop", "all")
+	@testset "develop" begin
+		include("./develop_tmp/febcc_2x2x2_pm/test.jl")
+		include("./develop_tmp/fept_tetragonal_2x2x2/test.jl")
+	end
 end
