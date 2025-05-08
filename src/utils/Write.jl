c508329d00d6a39dcf1079a856a907aa0d497eaf
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-05-08 12:29:07 +0900

    refactor: improve write_scecoeffs2xml function by simplifying XML element creation and enhancing coordinate formatting

--- src/utils/Write.jl
+++ src/utils/Write.jl
@@ -3,6 +3,7 @@ module Write
 using ..Version
 using ..Structures
 using EzXML
+using Printf
 
 """
     write_scecoeffs2xml(structure::Structure, filename::String)
@@ -23,8 +24,7 @@ function write_scecoeffs2xml(structure::Structure, filename::String)
 		root = setroot!(doc, ElementNode("Magesty"))
 
 		# Add version information
-		version_node = addelement!(root, "version")
-		addtext!(version_node, Version.version_string())
+		addelement!(root, "version", Version.version_string())
 
 		# Add system information
 		system_node = addelement!(root, "System")
@@ -37,29 +37,30 @@ function write_scecoeffs2xml(structure::Structure, filename::String)
 		lattice_node = addelement!(system_node, "LatticeVector")
 		for i in 1:3
 			vector = structure.supercell.lattice_vectors[i, :]
-			vector_str = join(string.(vector), " ")
+			vector_str = join([@sprintf("%.8e", x) for x in vector], " ")
 			addelement!(lattice_node, "a$i", vector_str)
 		end
 
 		# Convert boolean values to 1 and 0
-		periodicity_int = Int.(structure.supercell.periodicity)
+		periodicity_int = Int.(structure.is_periodic)
 		addelement!(system_node, "Periodicity", join(string.(periodicity_int), " "))
 
 		# Add atomic coordinates
 		positions_node = addelement!(system_node, "Positions")
-		for i in 1:structure.supercell.num_atoms
-			atom_node = addelement!(positions_node, "pos")
-			setattribute!(atom_node, "index", string(i))
-			setattribute!(atom_node, "element", structure.kd_name[structure.supercell.kd_int_list[i]])
-			coord_str = join(string.(structure.supercell.positions[i, :]), " ")
-			addtext!(atom_node, coord_str)
+		for (i, coord) in enumerate(eachcol(structure.supercell.x_frac))
+			coord_str = join([@sprintf("%.12f", x) for x in coord], " ")
+			atom_node = addelement!(positions_node, "pos", coord_str)
+			atom_node["index"] = string(i)
+			atom_node["element"] = structure.kd_name[structure.supercell.kd_int_list[i]]
 		end
 
 		# Write to file
-		write(filename, doc)
+		open(filename, "w") do f
+			EzXML.prettyprint(f, doc)
+		end
 	catch e
-		throw(SystemError("Failed to write XML file: $filename"))
+		throw(SystemError("Failed to write XML file: $filename.\nError: $e"))
 	end
 end
 
-end
+end # module Write
