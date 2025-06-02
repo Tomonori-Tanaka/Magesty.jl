f17ebc5a1d29eb938d1002a64587ebc47f79be1a
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-06-02 10:58:26 +0900

    fix: Update condition for ending magmom section in extract_energy_magmom function

--- tools/extract.jl
+++ tools/extract.jl
@@ -21,7 +21,7 @@ function extract_energy_magmom(
 				continue
 			end
 
-			if collecting && (occursin("DAV:", line) || occursin("RMM:", line))
+			if collecting && (occursin(":", line)) # end of magmom section (":" is intended to such as "DAV:" or "RMM:" keyword)
 				collecting = false
 				magmom_listoflist = deepcopy(magmom_temp_listoflist)
 				continue
