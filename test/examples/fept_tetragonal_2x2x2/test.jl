b2c85136c144ae2efec17e62ff0c5445d8cecde2
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-06-12 12:30:16 +0900

    test: Change test file output destination, add functionality to output system information to file, and remove unnecessary tests

--- test/examples/fept_tetragonal_2x2x2/test.jl
+++ test/examples/fept_tetragonal_2x2x2/test.jl
@@ -1,8 +1,18 @@
 using TOML
 
+io = open("./result/fept_tetragonal_2x2x2.txt", "w")
+
 input = TOML.parse(
-	open("/Users/tomorin/Packages/Magesty/test/examples/fept_tetragonal_2x2x2/input.toml", "r"),
+	open("./examples/fept_tetragonal_2x2x2/input.toml", "r"),
 )
 system = System(input, verbosity = false)
+
+# print info to file
+redirect_stdout(io) do
+	print_info(system)
+end
+
 sclus = SpinCluster(system, input, verbosity = false)
 Magesty.write_sce2xml(sclus, joinpath(@__DIR__, "scecoeffs.xml"))
+
+close(io)
