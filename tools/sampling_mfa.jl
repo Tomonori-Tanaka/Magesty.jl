d4964232aa57a1651aeaafd08c64c7dbd0895ec4
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-06-13 10:55:36 +0900

    feat: Refactor sampling_mfa function to improve error handling and support both MAGMOM and M_CONSTR keys

--- tools/sampling_mfa.jl
+++ tools/sampling_mfa.jl
@@ -22,37 +22,50 @@ Sample spin configurations using Mean Field Approximation.
 # Returns
 - Nothing, but writes sampled configurations to INCAR files
 """
-function sampling_mfa(input_path::AbstractString, τ::Real, num_sample::Int64, randomize::Bool, output_index::Int64, digits::Int64)
+function sampling_mfa(
+	input_path::AbstractString,
+	τ::Real,
+	num_sample::Int64,
+	randomize::Bool,
+	output_index::Int64,
+	digits::Int64,
+)
 	# Parse the INCAR file
 	incar = parse_incar(input_path)
-	m_constr = get(incar, :M_CONSTR, nothing)
-	if isnothing(m_constr)
-		error("M_CONSTR not found in INCAR file")
+
+	if :MAGMOM in keys(incar)
+		magmom = incar[:MAGMOM]
+	elseif :M_CONSTR in keys(incar)
+		magmom = incar[:M_CONSTR]
+	else
+		error("MAGMOM or M_CONSTR not found in INCAR file")
 	end
-	
-	if length(m_constr) % 3 != 0
+
+	if length(magmom) % 3 != 0
 		error("The length of M_CONSTR must be a multiple of 3")
 	end
-	
-	num_atoms = length(m_constr) ÷ 3
+
+	num_atoms = length(magmom) ÷ 3
 	# Read the spin configuration
 	spin_matrix = zeros(3, num_atoms)
 	for i in 1:num_atoms
-		spin_matrix[:, i] = m_constr[(3i-2):3i]
+		spin_matrix[:, i] = magmom[(3i-2):3i]
 	end
-	
+
 	# Sample the spin configurations
 	for i in 1:num_sample
 		output_spin_matrix = sampling_mfa_for_each_sample(spin_matrix, τ)
-		if randomize	# Randomize the quantization axis if specified
+		if randomize# Randomize the quantization axis if specified
 			output_spin_matrix = randomize_quantization_axis(output_spin_matrix)
 		end
 
 		# Write the spin configuration to the output file
 		output_spin_flattened = vec(output_spin_matrix)  # more efficient than output_spin_matrix[:]
 		output_incar = copy(incar)
-		output_incar[:M_CONSTR] = output_spin_flattened
 		output_incar[:MAGMOM] = output_spin_flattened
+		if :M_CONSTR in keys(incar)
+			output_incar[:M_CONSTR] = output_spin_flattened
+		end
 
 		# Write the INCAR file with sample number
 		output_path = @sprintf("sample-%0*d.INCAR", digits, output_index*num_sample + i)
@@ -75,19 +88,19 @@ Generate a single sample of spin configurations using von Mises-Fisher distribut
 function sampling_mfa_for_each_sample(input_spin_matrix::AbstractMatrix{<:Real}, τ::Real)
 	output_spin_matrix = zeros(size(input_spin_matrix))
 	for (i, spin) in enumerate(eachcol(input_spin_matrix))
-		if isapprox(norm(spin), 0.0, atol=1e-10)
+		if isapprox(norm(spin), 0.0, atol = 1e-10)
 			continue
 		end
 		# Prepare the mean direction and the concentration in the von Mises-Fisher distribution
 		mean_direction = normalize(spin)  # more efficient than spin / norm(spin)
-        if τ > 0.99999
-            concentration = 0.000001
-        elseif τ < 0.00001
-            return input_spin_matrix
-        else
-            m = thermal_averaged_m(τ)
-            concentration = 3m / τ
-        end
+		if τ > 0.99999
+			concentration = 0.000001
+		elseif τ < 0.00001
+			return input_spin_matrix
+		else
+			m = thermal_averaged_m(τ)
+			concentration = 3m / τ
+		end
 
 		# Calculate the von Mises-Fisher distribution
 		vmf = VonMisesFisher(mean_direction, concentration)
@@ -161,7 +174,7 @@ function rotation_matrix_from_vectors(v1::Vector{Float64}, v2::Vector{Float64})
 	c = dot(v1_normalized, v2_normalized)
 
 	# Skew-symmetric matrix
-	s = [        0 -v[3] v[2];
+	s = [         0 -v[3] v[2];
 		v[3] 0 -v[1];
 		-v[2] v[1] 0]
 
@@ -208,21 +221,21 @@ function main()
 		"--num_samples", "-n"
 		help = "The number of samples for each temperature"
 		arg_type = Int64
-        default = 1
+		default = 1
 
 		"--randomize", "-r"
 		help = "Randomize the quantization axis"
-		action = "store_true"	
+		action = "store_true"
 
 	end
 
 	args = parse_args(s)
-	
+
 	# Generate temperature range
 	temps = args["temp_start"]:args["temp_step"]:args["temp_end"]
 
 	digits = length(string(length(collect(temps)) * args["num_samples"]))
-	
+
 	for (i, τ) in enumerate(temps)
 		sampling_mfa(args["input"], τ, args["num_samples"], args["randomize"], i-1, digits)
 	end
@@ -232,7 +245,12 @@ end
 
 function print_info(args)
 	@printf("Input file: %s\n", args["input"])
-	@printf("Temperature range: %.2f to %.2f with step %.2f\n", args["temp_start"], args["temp_end"], args["temp_step"])
+	@printf(
+		"Temperature range: %.2f to %.2f with step %.2f\n",
+		args["temp_start"],
+		args["temp_end"],
+		args["temp_step"]
+	)
 	@printf("Number of samples per temperature: %d\n", args["num_samples"])
 	@printf("Randomize quantization axis: %s\n", args["randomize"] ? "Yes" : "No")
 end
