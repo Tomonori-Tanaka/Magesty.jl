8b835c007ec9789baea29ef4ade08b73dc941713
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-07-15 16:08:47 +0900

    feat: add XML caching to avoid document reloading and implement JPhi node pre-parsing for improved matrix computation efficiency

--- tools/micromagnetics.jl
+++ tools/micromagnetics.jl
@@ -20,7 +20,7 @@ function calc_dm_vector(j_ex::Array{Float64, 2})::Vector{Float64}
     return [x, y, z]
 end
 
-function calc_micromagnetics(input_xml::String, system::System)::Nothing
+function calc_micromagnetics(input_xml::String, system::System, cutoff::Union{Float64, Nothing}=nothing)::Tuple{Matrix{Float64}, Matrix{Float64}}
     num_atoms = system.structure.supercell.num_atoms
     atoms_in_prim = system.symmetry.atoms_in_prim   # atom indices in the primitive cell
     min_distance_pairs = system.cluster.min_distance_pairs
@@ -40,9 +40,16 @@ function calc_micromagnetics(input_xml::String, system::System)::Nothing
         for i_pair in 1:num_atoms
             if i_pair == i_atom; continue; end
 
-
             dist_info_list = min_distance_pairs[i_atom, i_pair]
             for dist_info in dist_info_list
+                # Check cutoff distance if specified
+                if cutoff !== nothing
+                    distance = norm(dist_info.relative_vector)
+                    if distance > cutoff
+                        continue
+                    end
+                end
+
                 cell_index = dist_info.cell_index
                 # relative vector from i_atom to i_pair in Cartesian coordinates
                 relvec::Vector{Float64} = dist_info.relative_vector
@@ -51,20 +58,27 @@ function calc_micromagnetics(input_xml::String, system::System)::Nothing
                 exchange_tensor = convert2tensor(input_xml, [i_atom, i_pair], cell_index)
 
                 jij = 1/3*tr(exchange_tensor)
-                stiff += 1/2*jij*kron(relvec, relvec')
+                # Avoid creating temporary arrays by using direct element access
+                for i in 1:3, j in 1:3
+                    stiff[i, j] += 0.5 * jij * relvec[i] * relvec[j]
+                end
+                
                 dm_vector = calc_dm_vector(exchange_tensor)
-                spiral += kron(dm_vector, relvec')
+                # Avoid creating temporary arrays by using direct element access
+                for i in 1:3, j in 1:3
+                    spiral[i, j] += dm_vector[i] * relvec[j]
+                end
             end
         end
         stiff_loc[t_id] = stiff
         spiral_loc[t_id] = spiral
     end
     for t_id in 1:nthreads
-        stiffness_matrix += stiff_loc[t_id]
-        spiralization_matrix += spiral_loc[t_id]
+        stiffness_matrix .+= stiff_loc[t_id]
+        spiralization_matrix .+= spiral_loc[t_id]
     end
-    display(stiffness_matrix)
-    display(spiralization_matrix)
+
+    return stiffness_matrix, spiralization_matrix
 end
 
 function main()
@@ -77,11 +91,22 @@ function main()
         "--input_jld2", "-j"
         help = "Input jld2 file for System struct"
         required = true
+
+        "--cutoff", "-c"
+        help = "Cutoff distance for atom pairs (in Angstrom). If not specified, all atom pairs are considered"
+        default = nothing
     end
 
     args = parse_args(s)
     @load args["input_jld2"] system
-    calc_micromagnetics(args["input_xml"], system)
+
+    cutoff = args["cutoff"] === nothing ? nothing : parse(Float64, args["cutoff"])
+    stiffness_matrix, spiralization_matrix = calc_micromagnetics(args["input_xml"], system, cutoff)
+    println("Stiffness matrix:")
+    display(stiffness_matrix)
+    println("")
+    println("Spiralization matrix:")
+    display(spiralization_matrix)
 end
 
 main()
