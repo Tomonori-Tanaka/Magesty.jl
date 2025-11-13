f4aef03994a65fb3fa3cb24bbe6e0dc9b0bb66fe
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-11-13 14:00:35 +0900

    feat: introduce SHSiteIndex and SHProduct structures for enhanced index management and operations

--- src/types/AtomicIndices.jl
+++ src/types/AtomicIndices.jl
@@ -8,7 +8,8 @@ export Indices, IndicesUniqueList, get_total_L, get_atom_l_list,
 	equivalent,
 	product_indices_of_all_comb,
 	product_indices,
-	indices_singleatom
+	indices_singleatom,
+	SHSiteIndex, SHProduct
 
 # ─────────────────────────────────────────────────────────────────────────────
 # Indices
@@ -81,7 +82,7 @@ function Base.convert(::Type{NTuple{4, Int}}, indices::Indices)
 			convert(Int, indices.atom),
 			convert(Int, indices.l),
 			convert(Int, indices.m),
-			convert(Int, indices.cell)
+			convert(Int, indices.cell),
 		)
 	catch e
 		throw(InexactError(:convert, NTuple{4, Int}, indices))
@@ -100,6 +101,48 @@ function show(io::IO, indices::Indices)
 	)
 end
 
+struct SHSiteIndex
+	i::Int
+	l::Int
+	m::Int
+
+	function SHSiteIndex(i::Integer, l::Integer, m::Integer)
+		if l < 1
+			throw(DomainError("l must be positive, got l = $l"))
+		elseif abs(m) > l
+			throw(DomainError("|m| must not exceed l, got m = $m for l = $l"))
+		end
+		new(convert(Int, i), convert(Int, l), convert(Int, m))
+	end
+end
+
+function SHSiteIndex(tuple::NTuple{3, Integer})
+	return SHSiteIndex(tuple...)
+end
+
+isless(shsi1::SHSiteIndex, shsi2::SHSiteIndex) =
+	(shsi1.i, shsi1.l, shsi1.m) < (shsi2.i, shsi2.l, shsi2.m)
+isless(shsi1::SHSiteIndex, tuple::NTuple{3, Integer}) = (shsi1.i, shsi1.l, shsi1.m) < tuple
+isless(tuple::NTuple{3, Integer}, shsi2::SHSiteIndex) = tuple < (shsi2.i, shsi2.l, shsi2.m)
+
+==(shsi1::SHSiteIndex, shsi2::SHSiteIndex) =
+	(shsi1.i, shsi1.l, shsi1.m) == (shsi2.i, shsi2.l, shsi2.m)
+==(shsi1::SHSiteIndex, tuple::NTuple{3, Integer}) = (shsi1.i, shsi1.l, shsi1.m) == tuple
+==(tuple::NTuple{3, Integer}, shsi2::SHSiteIndex) = tuple == (shsi2.i, shsi2.l, shsi2.m)
+
+hash(shsi::SHSiteIndex, h::UInt) = hash((shsi.i, shsi.l, shsi.m), h)
+
+function Base.convert(::Type{NTuple{3, Int}}, shsi::SHSiteIndex)::NTuple{3, Int}
+	try
+		return (convert(Int, shsi.i), convert(Int, shsi.l), convert(Int, shsi.m))
+	catch e
+		throw(InexactError(:convert, NTuple{3, Int}, shsi))
+	end
+end
+
+function show(io::IO, shsi::SHSiteIndex)
+	print(io, "(i: $(shsi.i), l: $(shsi.l), m: $(shsi.m))")
+end
 
 # ─────────────────────────────────────────────────────────────────────────────
 # IndicesUniqueList
@@ -270,12 +313,14 @@ function product_indices_of_all_comb(
 	cell_list::AbstractVector{<:Integer},
 )::Vector{IndicesUniqueList}
 	if length(atom_list) != length(lmax_list) != length(cell_list)
-		throw(ErrorException(
-			"Input vectors must have the same length. Got lengths: " *
-			"atom_list=$(length(atom_list)), " *
-			"lmax_list=$(length(lmax_list)), " *
-			"cell_list=$(length(cell_list))"
-		))
+		throw(
+			ErrorException(
+				"Input vectors must have the same length. Got lengths: " *
+				"atom_list=$(length(atom_list)), " *
+				"lmax_list=$(length(lmax_list)), " *
+				"cell_list=$(length(cell_list))",
+			),
+		)
 	end
 
 	list_tmp = Vector{Vector{Indices}}()
@@ -349,10 +394,61 @@ Generate all possible `Indices` for a single atom with given l value and cell in
 """
 function indices_singleatom(atom::Integer, l::Integer, cell::Integer)::Vector{Indices}
 	vec = Vector{Indices}()
-	for m in -l:l
+	for m in (-l):l
 		push!(vec, Indices(atom, l, m, cell))
 	end
 	return sort(vec)
+end
+
+
+struct SHProduct
+	data::Vector{SHSiteIndex}
+
+	function SHProduct()
+		new(Vector{SHSiteIndex}())
+	end
+
+	function SHProduct(vec::AbstractVector{SHSiteIndex})
+		new(unique(sort(vec)))
+	end
+
+	function SHProduct(shsi::SHSiteIndex)
+		new(Vector{SHSiteIndex}([shsi]))
+	end
+end
+
+getindex(shp::SHProduct, idx::Int) = shp.data[idx]
+length(shp::SHProduct) = length(shp.data)
+eltype(::Type{SHProduct}) = SHSiteIndex
+hash(shp::SHProduct, h::UInt) = hash(shp.data, h)
+iterate(shp::SHProduct, state::Int = 1) = iterate(shp.data, state)
+isempty(shp::SHProduct) = isempty(shp.data)
+size(shp::SHProduct) = size(shp.data)
+==(shp1::SHProduct, shp2::SHProduct) = shp1.data == shp2.data
+in(val::SHSiteIndex, shp::SHProduct) = val in shp.data
+
+function isless(shp1::SHProduct, shp2::SHProduct)
+	return length(shp1.data) < length(shp2.data) || shp1.data < shp2.data
+end
+
+function sort(shp::SHProduct)
+	return SHProduct(sort(shp.data))
+end
+
+function push!(shp::SHProduct, shsi::SHSiteIndex)
+	if shsi in shp
+		return shp
+	else
+		push!(shp.data, shsi)
+		return shp
+	end
+end
+
+function append!(shp::SHProduct, vec::AbstractVector{SHSiteIndex})
+	for shsi in vec
+		push!(shp, shsi)
+	end
+	return shp
 end
 
 end
