f750f371b9a2c8c942a5d2d7c2a72f3b116f936d
TomonoriTanaka <tanaka.t.bj3ac@gmail.com>
2025-12-03 14:29:22 +0900

    feat: update Basis module with new tesseral_linear_combos_from_tesseral_bases function and adjust LinearCombo validation logic; enhance tests for Basis functionality

--- test/component_test/Basis.jl
+++ test/component_test/Basis.jl
@@ -1,4 +1,4 @@
-@testitem "Basis" begin
+@testset "Basis" begin
 	include("../../src/utils/AngularMomentumCoupling.jl")
 	using .AngularMomentumCoupling
 	include("../../src/types/Basis.jl")
@@ -9,11 +9,22 @@
 		@testset "Constructor" begin
 			atoms = [10, 20]
 			ls = [1, 1]
-			bases, paths = build_all_complex_bases(ls)
+			lc_list = tesseral_linear_combos_from_tesseral_bases(ls, atoms)
+			@test length(lc_list) == 9
+			# Lf = 0
+			lc_lf0 = lc_list[1]
+			@test lc_lf0.ls == (1, 1)
+			@test lc_lf0.Lf == 0
+			@test lc_lf0.Lseq == Int[]
+			@test lc_lf0.atoms == [10, 20]
+			@test lc_lf0.coeff_list == [1.0]
+			@test isapprox(
+				lc_lf0.coeff_tensor,
+				[1/sqrt(3) 0.0 0.0; 0.0 1/sqrt(3) 0.0; 0.0 0.0 1/sqrt(3)],
+				atol = 1e-10,
+			)
 
-			keys = sort(collect(keys(bases)))   # [0, 1, 2]
 
-			# Lf = 0, Lseq = []
 		end
 	end
 end
