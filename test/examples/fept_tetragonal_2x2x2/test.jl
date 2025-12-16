c6da69bb04137feba15fe48eafa32bb913601425
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-12-16 22:37:02 +0900

    feat: implement write_xml function for System and Structure, enabling XML output of system information and enhancing data export capabilities

--- test/examples/fept_tetragonal_2x2x2/test.jl
+++ test/examples/fept_tetragonal_2x2x2/test.jl
@@ -8,6 +8,36 @@ input = TOML.parse(
 # Build SCE basis using build_sce_basis
 system = build_sce_basis(input, verbosity = false)
 
+# Write System to XML file
+system_xml_file = joinpath(@__DIR__, "system.xml")
+write_xml(system, system_xml_file)
+
+# Test: Compare basisset from XML with original basisset
+system_from_xml = build_sce_basis_from_xml(input, system_xml_file, verbosity = false)
+
+# Compare salc_list
+@assert length(system.basisset.salc_list) == length(system_from_xml.basisset.salc_list) "SALC list length mismatch"
+
+for (i, (salc_orig, salc_xml)) in enumerate(zip(system.basisset.salc_list, system_from_xml.basisset.salc_list))
+	@assert length(salc_orig) == length(salc_xml) "SALC group $i length mismatch"
+	
+	for (j, (cbc_orig, cbc_xml)) in enumerate(zip(salc_orig, salc_xml))
+		# Compare basic fields
+		@assert cbc_orig.ls == cbc_xml.ls "SALC[$i][$j]: ls mismatch"
+		@assert cbc_orig.Lf == cbc_xml.Lf "SALC[$i][$j]: Lf mismatch"
+		@assert cbc_orig.Lseq == cbc_xml.Lseq "SALC[$i][$j]: Lseq mismatch"
+		@assert cbc_orig.atoms == cbc_xml.atoms "SALC[$i][$j]: atoms mismatch"
+		@assert cbc_orig.multiplicity == cbc_xml.multiplicity "SALC[$i][$j]: multiplicity mismatch"
+		
+		# Compare coefficient vector (numerical)
+		@assert isapprox(cbc_orig.coefficient, cbc_xml.coefficient, atol=1e-10) "SALC[$i][$j]: coefficient mismatch"
+		
+		# Compare coeff_tensor (numerical)
+		@assert size(cbc_orig.coeff_tensor) == size(cbc_xml.coeff_tensor) "SALC[$i][$j]: coeff_tensor size mismatch"
+		@assert isapprox(cbc_orig.coeff_tensor, cbc_xml.coeff_tensor, atol=1e-10) "SALC[$i][$j]: coeff_tensor mismatch"
+	end
+end
+
 # Read spin configurations
 datafile = joinpath(@__DIR__, "EMBSET.dat")
 spinconfig_list = read_embset(datafile)
@@ -34,30 +64,4 @@ sclus = SpinCluster(
 
 xml_file = joinpath(@__DIR__, "scecoeffs.xml")
 Magesty.write_xml(sclus, xml_file)
-
-# Test: Compare basisset from XML with original basisset
-system_from_xml = build_sce_basis_from_xml(input, xml_file, verbosity = false)
-
-# Compare salc_list
-@assert length(system.basisset.salc_list) == length(system_from_xml.basisset.salc_list) "SALC list length mismatch"
-
-for (i, (salc_orig, salc_xml)) in enumerate(zip(system.basisset.salc_list, system_from_xml.basisset.salc_list))
-	@assert length(salc_orig) == length(salc_xml) "SALC group $i length mismatch"
-	
-	for (j, (cbc_orig, cbc_xml)) in enumerate(zip(salc_orig, salc_xml))
-		# Compare basic fields
-		@assert cbc_orig.ls == cbc_xml.ls "SALC[$i][$j]: ls mismatch"
-		@assert cbc_orig.Lf == cbc_xml.Lf "SALC[$i][$j]: Lf mismatch"
-		@assert cbc_orig.Lseq == cbc_xml.Lseq "SALC[$i][$j]: Lseq mismatch"
-		@assert cbc_orig.atoms == cbc_xml.atoms "SALC[$i][$j]: atoms mismatch"
-		@assert cbc_orig.multiplicity == cbc_xml.multiplicity "SALC[$i][$j]: multiplicity mismatch"
-		
-		# Compare coefficient vector (numerical)
-		@assert isapprox(cbc_orig.coefficient, cbc_xml.coefficient, atol=1e-10) "SALC[$i][$j]: coefficient mismatch"
-		
-		# Compare coeff_tensor (numerical)
-		@assert size(cbc_orig.coeff_tensor) == size(cbc_xml.coeff_tensor) "SALC[$i][$j]: coeff_tensor size mismatch"
-		@assert isapprox(cbc_orig.coeff_tensor, cbc_xml.coeff_tensor, atol=1e-10) "SALC[$i][$j]: coeff_tensor mismatch"
-	end
-end
 
