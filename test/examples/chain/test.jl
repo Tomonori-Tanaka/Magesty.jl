dec223de433abbb00e74df87c04d6afbac568dbe
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2025-12-25 21:28:11 +0900

    feat: add chain test inclusion in runtests.jl for expanded test coverage

---
+++ test/examples/chain/test.jl
@@ -0,0 +1,83 @@
+using TOML
+using Printf
+using Test
+using StaticArrays
+using Random
+using Magesty
+
+
+input = TOML.parse(open(joinpath(@__DIR__, "input.toml"), "r"))
+input["regression"]["datafile"] = joinpath(@__DIR__, "EMBSET.dat")
+
+@testset "Chain Tests" begin
+	# E = ∑_{i < j} Jij (e_i ⋅ e_j) , Jij = -1.0
+	system = System(input, verbosity = false)
+
+	spinconfig_list = Magesty.SpinConfigs.SpinConfig[]
+	fm_energy = -2.0
+	afm_energy = 2.0
+	fm_spin_directions =
+		[0.0 0.0;
+			0.0 0.0;
+			1.0 1.0]
+	afm_spin_directions =
+		[ 0.0 0.0;
+			 0.0 0.0;
+			-1.0 1.0]
+	# magfield is dummy
+	fm_local_magfield =
+		[   0.0 0.0;
+			0.0 0.0;
+			0.0 0.0]
+	afm_local_magfield =
+		[   0.0 0.0;
+			0.0 0.0;
+			0.0 0.0]
+	push!(
+		spinconfig_list,
+		Magesty.SpinConfigs.SpinConfig(
+			fm_energy,
+			[1.0, 1.0], # magmom_size_list
+			fm_spin_directions,
+			fm_local_magfield,
+		),
+	)
+	push!(
+		spinconfig_list,
+		Magesty.SpinConfigs.SpinConfig(
+			afm_energy,
+			[1.0, 1.0],
+			afm_spin_directions,
+			afm_local_magfield,
+		),
+	)
+
+
+	# regression with w = 0
+	optimize = Magesty.Optimize.Optimizer(
+		system.structure,
+		system.symmetry,
+		system.basisset,
+		0.0,
+		0.0,
+		0.0,
+		spinconfig_list,
+		verbosity = false,
+	)
+	spincluster = Magesty.SpinCluster(
+		system.structure,
+		system.symmetry,
+		system.cluster,
+		system.basisset,
+		optimize,
+	)
+	path = joinpath(@__DIR__, "chain.xml")
+	Magesty.write_xml(spincluster, path)
+
+
+	@test spincluster.optimize.reference_energy ≈ 0.0 atol = 1e-6
+	@test length(spincluster.optimize.SCE) == 1
+	@test spincluster.optimize.SCE[1]*√(3) ≈ -1.0 atol = 1e-6
+
+
+end
