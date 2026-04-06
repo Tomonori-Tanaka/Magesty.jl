9e1298a13a705c76ea78f43d361d20fffcf1798e
T.Tanaka <tomonori.tanaka.academic@gmail.com>
2026-04-06 15:42:18 +0900

    refactor: enable verbose output for test sets and clean up unused display statements in dimer and square lattice tests

--- test/examples/square_lattice/test.jl
+++ test/examples/square_lattice/test.jl
@@ -7,11 +7,15 @@ using Magesty
 
 
 input = TOML.parse(open(joinpath(@__DIR__, "input.toml"), "r"))
-input["regression"]["datafile"] = joinpath(@__DIR__, "EMBSET.dat")
+# input["regression"]["datafile"] = joinpath(@__DIR__, "EMBSET.dat")
 
 @testset "Square Lattice Tests" begin
 	system = System(input, verbosity = false)
 	Magesty.write_xml(system, joinpath(@__DIR__, "system.xml"))
 
+	@testset "Hypothetical SCE model" begin
+		# Assign Jij = -1.0 eV (ferromagnetic interaction) to all pairs.
+		# Jij is the Heisenberg exchange interaction.
+	end
 
 end
